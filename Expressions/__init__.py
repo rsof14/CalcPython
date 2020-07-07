2696aea13fa53c53f657da9b584cd947171f7360
Dmitry Svalov <me@agentd.ru>
2020-07-07 12:39:23 +0500

    Initial commit

---
+++ Expressions/__init__.py
@@ -0,0 +1,7 @@
+from .expression import Expression
+from .number_expr import NumberExpr
+from .binary_operation import BinaryOperation
+from .add_operation import AddOperation
+from .subtract_operation import SubtractOperation
+from .multiply_operation import MultiplyOperation
+from .divide_operation import DivideOperation
